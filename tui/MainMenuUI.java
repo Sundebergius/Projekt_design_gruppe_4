0c849ebd51d8524afc593556c6cb8930ddf60e02
Mathias Søndberg-Madsen <1089441@ucn.dk>
2021-04-12 10:32:46 +0200

    Tilføjet MainMenuUI klassen

---
+++ tui/MainMenuUI.java
@@ -0,0 +1,35 @@
+package tui;
+
+
+/**
+ * Lav en beskrivelse af klassen MainMenuUI her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class MainMenuUI
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen MainMenuUI
+     */
+    public MainMenuUI()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int sampleMethod(int y)
+    {
+        // indsæt din egen kode her
+        return x + y;
+    }
+}
