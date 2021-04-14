d418b9ba8f89d31e0a98b20a93cc482c3c9d2ec2
Sundeberg <Soendberg1994@gmail.com>
2021-04-14 14:25:27 +0200

    Startede på 'TryMe' klassen

---
+++ tui/TryMe.java
@@ -0,0 +1,38 @@
+package tui;
+import model.*;
+
+/**
+ * Lav en beskrivelse af klassen TryMe her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class TryMe
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    PersonContainer personContainer;
+    LPContainer lpContainer; 
+
+    /**
+     * Konstruktør for objekter af klassen TryMe
+     */
+    public TryMe()
+    {
+        // initialiser instansvariable
+        personContainer = PersonContainer.getInstance();
+        lpContainer = LPContainer.getInstance();
+    }
+
+    /**
+     generate the data of the TryMe class. 
+     */
+    public void generateData()
+    {
+        // indsæt din egen kode her
+        Person tester = new Person("Mads Mikkelsen", "Sofiendalsvej 60", "9200", "Aalborg SV", "12345678");
+        personContainer.addPerson(tester); 
+        LP record = new LP();
+        Copy copy = new Copy(); 
+        
+    }
+}
